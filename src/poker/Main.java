99949aca15d9fb0dbb9e1179433e638213dbeb85
benjamin <git@benjamin.ie>
2017-02-06 23:45:29 +0000

    DeckofCards, tested in main class

--- src/poker/Main.java
+++ src/poker/Main.java
@@ -1,18 +1,39 @@
 package poker;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by benjamin kelly on 27/01/2017.
  * benjamin.kelly.1@ucdconnect.ie
  * student number: 14700869
  */
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
-
 
     public static void main(String[] args) {
         System.out.println("main class!");
 
+        DeckOfCards deck = new DeckOfCards();
+        deck.reset();
+        deck.shuffle();
+	    List<PlayingCard> player1 = new ArrayList<PlayingCard>();
+	    List<PlayingCard> player2 = new ArrayList<PlayingCard>();
+	    for (int i = 0; i < 5; i++) {
+		    player1.add(deck.dealNext());
+		    player2.add(deck.dealNext());
+	    }
+	    System.out.println(player1);
+	    System.out.println(player2);
+	    System.out.println(deck.size());
+
+	    for (int i = 0; i < 5; i++) {
+	    	deck.returnCard(player1.get(0));
+	    	deck.returnCard(player2.get(0));
+	    	player1.remove(0);
+	    	player2.remove(0);
+	    }
+	    System.out.println(deck.size());
+	    System.out.println(deck);
     }
 }
